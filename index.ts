4bfd36b0e136571ab0b5b3dafa39fd3ddef40434
daryl0313 <djbaten@gmail.com>
2019-12-29 20:45:09 -0600

    Solved day2 part 1

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 // Import stylesheets
 import "./style.css";
 
-import { getAnswer } from "./src/day1";
+import { getAnswer } from "./src/day2";
 
 async function writeAnswer() {
   // Write TypeScript code!
   const appDiv: HTMLElement = document.getElementById("app");
-  appDiv.innerHTML = `<h1>${await getAnswer()}</h1>`;
+  appDiv.innerHTML = `<pre>${JSON.stringify(await getAnswer(), undefined, 2)}</pre>`;
 }
 
 writeAnswer();
---
+++ src/day2.ts
@@ -0,0 +1,47 @@
+import { input } from "./day2.input";
+
+type IntOperation = "add" | "multiply" | "END";
+
+function getOperation(intCode: number): IntOperation {
+  switch (intCode) {
+    case 1:
+      return "add";
+    case 2:
+      return "multiply";
+    case 99:
+      return "END";
+    default:
+      throw new Error("Unknown IntCode");
+  }
+}
+
+// Part 1
+export async function getAnswer() {
+  // const input = "1,1,1,4,99,5,6,0,99";
+  const inputs = input.split(",").map(i => +i);
+
+  inputs[1] = 12;
+  inputs[2] = 2;
+
+  let i = 0;
+  do {
+    const operation = getOperation(inputs[i]);
+    if (operation === "END") {
+      i = null;
+      break;
+    }
+    const input1Index = inputs[i + 1];
+    const input2Index = inputs[i + 2];
+    const writeIndex = inputs[i + 3];
+    i += 4;
+
+    if (operation === "add") {
+      inputs[writeIndex] = inputs[input1Index] + inputs[input2Index];
+    } else {
+      inputs[writeIndex] = inputs[input1Index] * inputs[input2Index];
+    }
+  } while (i);
+  return inputs;
+}
+
+// Part 2
